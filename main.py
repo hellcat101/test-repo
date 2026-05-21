1d8278c218866c6ab967242ad62a21015d90ade0
hellcat101 <ishanigurung@gmail.com>
2026-05-21 13:48:13 +0200

    feat:double it module added(JIT1122-4).

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-def prod_integers(num1, num2):
-    return num1*num2
+# Main.py
 
-print(prod_integers(1,2))
+"""This module for blablabla"""
+
+import double_it    # first way to import
+
+# from double_it import double_it
+
+X_INIT: int = 1
+
+
+def prod_integers(old_inventory: int, new_inventory: int
+                  ) -> int:
+    """_summary_
+
+    :param num1: _description_
+    :type num1: dict[str, int]
+    :param num2: _description_
+    :type num2: list[int]
+    :return: _description_
+    :rtype: int
+    """
+
+    y = old_inventory + new_inventory + X_INIT
+
+    result = double_it.double_int(y)
+
+    return result
+
+
+if __name__ == "__main__":
+    print(prod_integers(1, 2))
